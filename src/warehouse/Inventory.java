8af0a046357f74f42adb7769d164f544c269ec2b
Lindsay Simpkins <LWSimpkins@users.noreply.github.com>
2014-09-06 19:13:53 -0400

    Fixed issue in inventory processing
    Needed to change while loop to do-while

--- src/warehouse/Inventory.java
+++ src/warehouse/Inventory.java
@@ -74,9 +74,13 @@ public class Inventory {
                         stack.pushItem(widget);
                         
                         System.out.println("New Stack: \n" + stack.toString());
-
-                        if (currentOrder != null) {
-                            //output = fillOrder(output);
+                        
+                        boolean a = currentOrder != null;
+                        boolean b = queue.isEmpty()==false;
+                        System.out.println(a + " " + b);
+                        if (currentOrder != null || queue.isEmpty()==false) {
+                            output = fillOrder(output);
+                            System.out.println("fillOrder was called from shipments");
                         }
                         break;
 
@@ -101,7 +105,8 @@ public class Inventory {
                         System.out.println("New queue: \n" + queue.toString());
 
                         if (stack.isEmpty() == false) {
-                            //output = fillOrder(output);
+                            output = fillOrder(output);
+                            System.out.println("fillOrder was called from orders");
                         }
                         break;
                 }
@@ -111,25 +116,32 @@ public class Inventory {
         }
 
         if (currentOrder != null) {
-            output += "Order for vendor " + currentOrder.getVendor() + " of " + currentOrder.getAmount() + " widgets cannot be filled";
+            output += "Order for vendor " + currentOrder.getVendor() + " of " + currentOrder.getAmount() + " widgets cannot be filled\n";
+            output += "Remaining widgets: \n" + listOfWidgetsUsed.toString();
 
             System.out.println("Order for vendor " + currentOrder.getVendor() + " of " + currentOrder.getAmount() + " widgets cannot be filled");
+            System.out.println("Remaining widgets: \n" + listOfWidgetsUsed.toString());
         }
         return output;
     }
 
     private String fillOrder(String output) {
 
-        while (queue.isEmpty() == false && stack.isEmpty() == false) {
+        do {
 
             if (currentOrder == null) {
                 currentOrder = queue.getItem();
                 numWidgetsNeeded = currentOrder.getAmount();
+                
+                System.out.println("Widgets needed: " + numWidgetsNeeded + " From: " + currentOrder.toString());
+                System.out.println("New Queue:\n" + queue.toString());
             }
 
             while (numWidgetsNeeded != 0 && stack.isEmpty() == false) {
                 Widget widgetFromStack = stack.popItem();
                 int numWidgetsFromStack = widgetFromStack.getAmount();
+                
+                System.out.println("Num Widgets from Stack: " + numWidgetsFromStack);
 
                 //If there are more widgets than needed for the order
                 //Create Widget object with widgets needed for the order, add to ArrayList
@@ -137,15 +149,17 @@ public class Inventory {
                 //Update number of unused widgets, and return to stack                
                 if (numWidgetsFromStack > numWidgetsNeeded) {
                     Widget widgetsForOrder = new Widget(numWidgetsNeeded, widgetFromStack.getCost(), widgetFromStack.getVendor());
-                    costOfOrder = numWidgetsNeeded * (widgetFromStack.getCost() * 1.5);
+                    costOfOrder += numWidgetsNeeded * (widgetFromStack.getCost() * 1.5);
                     listOfWidgetsUsed.add(widgetsForOrder);
                     widgetFromStack.setAmount(numWidgetsFromStack - numWidgetsNeeded);
                     stack.pushItem(widgetFromStack);
+                    
+                    System.out.println("Num > Need: $" + costOfOrder);
+                    System.out.println("New Stack\n" + stack.toString());
+                    
                     output = orderComplete(output);
-
-                    System.out.println("After completed order had > need " + stack.toString() + queue.toString());
-                    System.out.println(output);
-                } //If the widgets available is exactly the amount needed
+                    
+                    } //If the widgets available is exactly the amount needed
                 //pull shipment from stack and add to ArrayList
                 //update the cost of the order
                 //Update the numWidgetsNeeded
@@ -154,9 +168,10 @@ public class Inventory {
                     listOfWidgetsUsed.add(widgetFromStack);
                     output = orderComplete(output);
 
-                    System.out.println("After completed order had = need " + stack.toString() + queue.toString());
-                    System.out.println(output);
-                } //If there are fewer widgets available than the amount needed
+                    System.out.println("Num = Need: $" + costOfOrder);
+                    System.out.println("New Stack\n" + stack.toString());
+                } 
+                //If there are fewer widgets available than the amount needed
                 //pull shipment from stack and add to ArrayList
                 //update the cost of the order
                 //Update the numWidgetsNeeded
@@ -164,16 +179,20 @@ public class Inventory {
                     costOfOrder += numWidgetsFromStack * (widgetFromStack.getCost() * 1.5);
                     listOfWidgetsUsed.add(widgetFromStack);
                     numWidgetsNeeded -= numWidgetsFromStack;
+                    
+                    System.out.println("Num < Need: $" + costOfOrder + " Still need " + numWidgetsNeeded);
+                    
+                    System.out.println("New Stack\n" + stack.toString());
                 }
             }
-        }
+        } while (queue.isEmpty() == false && stack.isEmpty() == false);
 
         return output;
     }
 
     //output to string, empty ArrayList, clear currentOrder, cost, widgetsNeeded
     private String orderComplete(String output) {
-        output += String.format("%s \t %d \t %.2f", currentOrder.getVendor(), currentOrder.getAmount(), costOfOrder) + eol;
+        output += String.format("%s \t %d \t $%.2f", currentOrder.getVendor(), currentOrder.getAmount(), costOfOrder) + eol;
         while (listOfWidgetsUsed.isEmpty() == false) {
             Widget tempWidget = listOfWidgetsUsed.remove(0);
             output += tempWidget.toString() + eol;
@@ -182,6 +201,9 @@ public class Inventory {
         currentOrder = null;
         numWidgetsNeeded = 0;
         costOfOrder = 0;
+        
+        System.out.println("Order complete: " + numWidgetsNeeded + " $" + costOfOrder);
+        System.out.println("Output String: \n" + output);
 
         return output;
     }
